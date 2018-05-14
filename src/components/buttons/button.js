724b795155f6da2c40dd5c2934a6a0f32f664335
Matheus Valeriano <math.valeriano@gmail.com>
2018-05-14 19:51:49 -0300

    removes children

--- src/components/buttons/button.js
+++ src/components/buttons/button.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ text, handleClick, ...props }) => (
+const Button = ({ text, handleClick }) => (
   <button
     className="button"
     onClick={handleClick}
   >
-    {props.children}
     {text}
   </button>
 );
